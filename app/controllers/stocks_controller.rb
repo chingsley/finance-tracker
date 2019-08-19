6025bb3b6fdbd35b6b625f63a6921921ad28ae4e
chingsley <eneja.kc@gmail.com>
2019-08-19 11:59:05 +0100

    - implement user_stock many-to-many associations

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -4,6 +4,7 @@ class StocksController < ApplicationController
     if params[:stock].blank?
       flash.now[:danger] = "Stock symbol cannot be empty string."
     else
+      # @stock = Stock.new_from_lookup(params[:stock])
       @stock = { ticker: 'stock ticker', name: 'stock name', price: 'stock price' }
       flash.now[:danger] = "Invalid stock symbol. Please enter a correct stock symbol" unless @stock
     end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,14 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
 
   def self.new_from_lookup(ticker_symbol)
     begin
-      # StockQuote::Stock.new(api_key: 'Tpk_2beaf38fa2f24a30a25addad2927e565')
-      # client = IEX::Api::Client.new(
-      #   publishable_token: 'pk_de4f14bd5e9b48deada533b0e7061cdd',
-      #   endpoint: 'https://cloud.iexpais.com'
-      # )
-      # looked_up_stock = client.quote(ticker_symbol)
-      # new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.l)
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
       price = strip_commas(looked_up_stock.l)
       new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
