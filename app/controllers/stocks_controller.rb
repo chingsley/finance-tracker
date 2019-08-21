3312507cfae04a118f4f0d26ec4f3baffc82afaa
chingsley <eneja.kc@gmail.com>
2019-08-21 10:05:12 +0100

    - enable users add friends to their friends listing - users can click on view profile to see a list of stocks being tracked by a friend

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -13,4 +13,5 @@ class StocksController < ApplicationController
       format.js { render partial: 'users/result' }
     end
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,36 @@ class User < ApplicationRecord
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
+
+  def self.search(param)
+    param.strip!
+    param.downcase!
+    results = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq #the .unique method will remove all duplicate from the result
+    return nil unless results
+    results
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+  
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+  
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    User.where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users) # this is not a class level method, we'll call this on an instance of this class. This means that this method can accessed outside this class by calling it on an instance of this class
+    users.reject { |user| user.id == self.id }
+  end
+
+  def not_friends_with?(friend)
+    friendships.where(friend_id: friend.id).count < 1
+  end
 end
